443b12d690ed23c952675049fedfeb8d24c2eb3c
Saurabh_Shivam <60539011+Saurabh-Shivam@users.noreply.github.com>
2024-01-08 11:49:34 +0530

    50% fixed labContainer with th help of JS

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const notifiBar = document.querySelector(".notificationBar");
 function handleHide() {
   notifiBar.style.display = "none";
 }
+
+const labLink = document.querySelector("#labTag");
+const labContainer = document.querySelector(".mainLabContainer" );
+
+labLink.addEventListener("mouseover", () => {
+  labContainer.classList.add("active")
+} )
+
+labLink.addEventListener("mouseout", () => {
+  labContainer.classList.remove("active")
+} )
